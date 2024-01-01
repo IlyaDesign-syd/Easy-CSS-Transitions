843f94e4a5b5fef40ce59974fd5189eaa6e06d91
Miller <milleri@news.com.au>
2024-01-01 18:43:30 +1100

    React overhaul

--- easy-css-transitions/src/App.jsx
+++ easy-css-transitions/src/App.jsx
@@ -7,28 +7,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-[min(90vw,_90vh)] h-[min(90vw,_90vh)]'>
+      <div className='bg-slate-700 rounded-sm relative w-[100%] h-[100%]'>
+        <div className='bg-yellow-500 absolute rotate-45 w-[12%] h-[12%] left-1/4 top-1/2'></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
 
